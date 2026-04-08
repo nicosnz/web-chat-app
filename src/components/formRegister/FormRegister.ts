2795e0b740764789c59766005509a162c127be42
Emanuel Oly <olysancheznicolas@gmail.com>
2026-04-07 20:18:08 -0400

    bem para login y register

--- src/components/formRegister/FormRegister.ts
+++ src/components/formRegister/FormRegister.ts
@@ -9,16 +9,16 @@ export class FormRegister extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
             <form class="form-register">
-                <h2 class="form_title-register">Registrate</h2>
-                <input class="form_input-register" type="email" placeholder="Correo electrónico">
-                <input class="form_input-register" type="text" placeholder="Login">
-                <input class="form_input-register" type="text" placeholder="Nombre">
-                <input class="form_input-register" type="text" placeholder="Apellido">
-                <input class="form_input-register" type="number" placeholder="Teléfono">
-                <input class="form_input-register" type="password" placeholder="Password">
-                <input class="form_input-register" type="password" placeholder="Password (Nuevamente)">
-                <input class="form_input_submit-register" type="submit" value="Registrate">
-                <a href="/login"  class="form_link-register">¿Iniciar sesión?</a>
+                <h2 class="form-register__title">Registrate</h2>
+                <input class="form-register__input" type="email" placeholder="Correo electrónico">
+                <input class="form-register__input" type="text" placeholder="Login">
+                <input class="form-register__input" type="text" placeholder="Nombre">
+                <input class="form-register__input" type="text" placeholder="Apellido">
+                <input class="form-register__input" type="number" placeholder="Teléfono">
+                <input class="form-register__input" type="password" placeholder="Password">
+                <input class="form-register__input" type="password" placeholder="Password (Nuevamente)">
+                <input class="form-register__input_submit" type="submit" value="Registrate">
+                <a href="/login"  class="form-register__link">¿Iniciar sesión?</a>
             
             </form>
         
